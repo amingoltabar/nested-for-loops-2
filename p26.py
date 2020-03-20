def153d8f6e035e547d2d5695b70b313278f7a3f
amingoltabar <61889677+amingoltabar@users.noreply.github.com>
2020-03-20 18:38:07 +0330

    Add files via upload

---
+++ p26.py
@@ -0,0 +1,6 @@
+for i in range(0,3,10):
+    for j in range(0,6,1):
+        for k in range(0,11,1):
+            if i*50+j*20+k*10==100:
+                print(i,'50$ bill(s)',j,'20$ bill(s)',k,'10$ bill(s)')
+                
